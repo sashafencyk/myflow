16ed03c3e39344e31699906413d581757fde3866
Sasha Fentsyk <sasha.fencyk@gmail.com>
2022-07-20 22:35:42 +0200

    init

---
+++ myflow/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+var statusProcess = Process.Start("git", "status");
+var test = await statusProcess.StandardOutput.ReadToEndAsync();
+Console.WriteLine(test);
